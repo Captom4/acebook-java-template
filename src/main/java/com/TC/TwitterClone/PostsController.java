340a4868cd456acd1d0669f7dfd776d9c04efe2b
Austen Grice-Griffin <austen.grice-griffin@Austens-MacBook-Pro.local>
2018-12-03 15:15:59 +0000

    Creating a post

--- src/main/java/com/TC/TwitterClone/PostsController.java
+++ src/main/java/com/TC/TwitterClone/PostsController.java
@@ -1,11 +1,10 @@
 package com.TC.TwitterClone;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/posts")
@@ -16,10 +15,23 @@ public class PostsController {
         messageContent = new ArrayList<>();
 
         messageContent.add(new Posts("TestUser", "Hello this is my test post on acebook!"));
+        messageContent.add(new Posts("user", "Hello this is my test post on acebook!"));
 
     }
     @RequestMapping(value = "/all", method = RequestMethod.GET )
     public List<Posts> getAll(){
+        return messageContent;
+    }
+
+    @RequestMapping(value = "/user/{username}", method = RequestMethod.GET)
+    public List<Posts> getUsername(@PathVariable String username){
+        return messageContent.stream().filter(x -> x.getUsername() == username)
+                .collect(Collectors.toList());
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public List<Posts> create (@RequestBody Posts post){
+        messageContent.add(post);
         return messageContent;
     }
 }
