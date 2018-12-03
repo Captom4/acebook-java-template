3ee9fe002b427d2f61c35b91992b20a08232f8b3
Tom Hird <tombjh@hotmail.co.uk>
2018-12-03 22:39:35 +0000

    Revert "Signing up feature"
    This reverts commit 1f8f4854c003ea5300d9564b824dfd41427aa0dc.

--- src/main/java/com/TC/TwitterClone/SignUpRepository.java
+++
@@ -1,8 +0,0 @@
-package com.TC.TwitterClone;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SignUpRepository extends JpaRepository<SigningUp, Long> {}
