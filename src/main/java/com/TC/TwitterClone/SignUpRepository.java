62606dccbb310aad1de21e8e02cd6b383f6aaf59
maxfrancis1 <max.francis2000@gmail.com>
2018-12-06 14:35:30 +0000

    Changing

--- src/main/java/com/TC/TwitterClone/SignUpRepository.java
+++ src/main/java/com/TC/TwitterClone/SignUpRepository.java
@@ -1,8 +1,8 @@
 package com.TC.TwitterClone;
 
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+        import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SignUpRepository extends JpaRepository<SigningUp, Long> {}
